e87d8c587bde6e5cdb4c6c93ea645e5badccea56
SimonLo <570651149@qq.com>
2016-03-03 10:12:11 +0800

    添加了dog类

---
+++ PublicInfoApp/PublicInfoApp/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  PublicInfoApp
+//
+//  Created by SimonLo on 3/3/16.
+//  Copyright © 2016年 SimonLo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ PublicInfoApp/PublicInfoApp/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  PublicInfoApp
+//
+//  Created by SimonLo on 3/3/16.
+//  Copyright © 2016年 SimonLo. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
